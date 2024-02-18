8846adfbcae4a5c0ea7b922d803c4eeae9434432
Merkulov Sergey <sergeinder@mail.ru>
2024-02-18 13:01:23 +0300

    Add favicon and possibility to add images and css file from our directly from our project. Upgrage register and add CSS file for it

--- app/src/main.py
+++ app/src/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.pages.router import router as router_pages
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="app/static"), "static")
 
 app.include_router(router_pages)
