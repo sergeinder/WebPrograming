fb202fc31d8cbd21525dc08a776d65b584de8c4f
Merkulov Sergey <sergeinder@mail.ru>
2024-03-03 18:13:20 +0300

    Add API from YandexMap to page with Moscow

--- app/src/main.py
+++ app/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+import requests
 
 from app.pages.router import router as router_pages
 
@@ -8,3 +9,13 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="app/static"), "static")
 
 app.include_router(router_pages)
+
+
+@app.get('/get_first_user')
+def first_user():
+    api_url = "https://jsonplaceholder.typicode.com/users"
+    all_users = requests.get(api_url).json()
+    user1 = all_users[0]
+    name = user1["name"]
+    email = user1["email"]
+    return {'name': name, "email": email}
