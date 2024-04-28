6bf306a471b6e13101707e4230b76391fd45daa5
Merkulov Sergey <sergeinder@mail.ru>
2024-04-28 19:58:48 +0300

    Trying in websocket

--- app/cities/router.py
+++ app/cities/router.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from typing import List
+
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 
 from app.cities.dao import CitiesDAO
 from app.cities.scemas import SCities
@@ -7,6 +9,25 @@ router = APIRouter(
     prefix="/cities",
     tags=["Города"]
 )
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
 
 
 @router.get("")
@@ -18,3 +39,13 @@ async def get_cities() -> list[SCities]:
 async def get_cities_by_id(city_id: int) -> SCities:
     return await CitiesDAO.find_by_id(city_id)
 
+
+@router.websocket("/ws/{city_id}")
+async def websocket_endpoint(websocket: WebSocket, city_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.broadcast(f"{city_id}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
